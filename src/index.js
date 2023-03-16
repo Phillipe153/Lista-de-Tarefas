1970734a25a7a98d2e7c7190ba472389626d38af
Phillipe153 <rodrigues153@gmail.com>
2023-03-16 08:16:00 -0300

    chore: implementa config.json, e as pastas de models, migrations, seeders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { app } =  require('./app')
-const PORT = process.env.MYSQL_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Listenig on port ${PORT}`)
