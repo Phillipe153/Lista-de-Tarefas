3007d5ed45a1336dd53f3ced6dd845edb4edf214
Phillipe153 <rodrigues153@gmail.com>
2023-03-21 11:18:19 -0300

    test: implementa arquivo de testes

---
+++ src/tests/mockTarefas.js
@@ -0,0 +1,28 @@
+const mockTarefas = [
+	{
+		"tarefa": "terminar o projeto Tarefas",
+		"prazos": {
+			"status": "pendente",
+			"prioridade": "urgente",
+			"prazo": "16/03/2023",
+		}
+	},
+	{
+		"tarefa": "Candidatar-se a vagas backend",
+		"prazos": {
+			"status": "pendente",
+			"prioridade": "nao urgente",
+			"prazo": "16/03/2023",
+		}
+	},
+	{
+		"tarefa": "Terminar o projeto1",
+		"prazos": {
+			"status": "concluido",
+			"prioridade": "urgente",
+			"prazo": "16/03/2023",
+		}
+	}
+]
+
+module.exports = { mockTarefas }
---
+++ src/tests/teste.test.js
@@ -0,0 +1,79 @@
+const chai = require('chai');
+const { expect } = chai
+const chaiHttp = require('chai-http');
+const { app } = require('../app')
+const sinon = require('sinon');
+const service = require('../service/service');
+const mockTarefas = require('./mockTarefas');
+// const fs = require('fs');
+
+chai.use(chaiHttp);
+
+
+describe('Teste de status da tarefa', () => {
+  before(() => {
+    sinon.stub(service, 'getService').resolves(mockTarefas);
+  });
+  after(() => {
+    service.getService.restore()
+  })
+    describe ('teste da tarefa 1', () => {
+        it('testa se retorna um tarefa pendente', async () => {
+          const { body: {mockTarefas} } = await chai.request(app).get('/get');
+
+          expect(mockTarefas[0].prazos.status).to.be.equal('pendente');
+          });
+        it ('testa se tem a propriedade tarefa', async ()  => {
+          const { body: {mockTarefas} } = await chai.request(app).get('/get');
+
+          expect(mockTarefas[0]).to.have.property('tarefa');
+
+        })
+        it ('testa se tem a propriedade prazos', async () => {
+          const { body: {mockTarefas} } = await chai.request(app).get('/get');
+
+          expect(mockTarefas[0]).to.have.property('prazos');
+          
+        })
+        it ('testa o nome da terefa', async () => {
+          const { body: {mockTarefas} } = await chai.request(app).get('/get');
+
+          expect(mockTarefas[0].tarefa).to.be.equal('terminar o projeto Tarefas');
+          
+        })
+        
+  })
+
+  describe ('teste da tarefa 2', () => {
+    it ('testa o nome da terefa', async () => {
+      const { body: {mockTarefas} } = await chai.request(app).get('/get');
+
+      expect(mockTarefas[1].tarefa).to.be.equal('Candidatar-se a vagas backend');
+      
+    })
+    it('testa se retorna um tarefa nao urgente', async () => {
+      const {  body: {mockTarefas} } = await chai.request(app).get('/get');
+    
+      expect(mockTarefas[1].prazos.prioridade).to.be.equal('nao urgente')
+    });
+   
+    
+})
+
+describe ('teste da tarefa 3', () => {
+  it ('testa o nome da terefa', async () => {
+    const { body: {mockTarefas} } = await chai.request(app).get('/get');
+
+    expect(mockTarefas[2].tarefa).to.be.equal('Terminar o projeto1');
+    
+  })
+  it('testa se retorna uma tarefa concluida', async () => {
+    const {  body: {mockTarefas} } = await chai.request(app).get('/get');
+  
+    expect(mockTarefas[2].prazos.status).to.be.equal('concluido')
+  });
+  
+})
+
+
+});
